72152ab3da8130a00d98b33a0476930f3a7b06e1
Yudi Yamane <yudiyamane@gmail.com>
2020-04-26 20:12:21 -0300

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello world</h1>;
-}
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
